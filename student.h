3b30fc425e5e8a764a4c48d7a433b315961c4f8d
Uday Kiran <udaykiranuk1126@gmail.com>
2026-01-13 16:08:06 +0530

    Add initial project files and structure
    - Create .gitignore to exclude .vscode directory
    - Add empty info.txt and main.c files
    - Implement student.h header file with Student struct definition
    - Create student.c file and include student.h

---
+++ student.h
@@ -0,0 +1,15 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+typedef struct
+{
+    char id[10];
+    char name[50];
+    float s1_score;
+    float s2_score;
+    float s3_score;
+    float s4_score;
+    float s5_score;
+} Student;
+
+#endif
