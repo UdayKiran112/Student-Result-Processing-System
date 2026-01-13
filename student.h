a1f90360802ea69aecde29867250d763eb5939f9
Uday Kiran <udaykiranuk1126@gmail.com>
2026-01-13 16:10:16 +0530

    Refactor Student struct to use an array for scores

--- student.h
+++ student.h
@@ -5,11 +5,7 @@ typedef struct
 {
     char id[10];
     char name[50];
-    float s1_score;
-    float s2_score;
-    float s3_score;
-    float s4_score;
-    float s5_score;
+    float scores[5];
 } Student;
 
 #endif
