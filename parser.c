b4de052003d0831205f7760e75e3f51cbba816fb
Uday Kiran <udaykiranuk1126@gmail.com>
2026-01-14 08:39:34 +0530

    Enhance input validation for student names and improve formatting in parser

--- parser.c
+++ parser.c
@@ -20,16 +20,15 @@ void parse_input(int count, FILE *input, FILE *output)
 
         int idCheck = checkID(line, students, n);
 
-        if(idCheck == -1)
+        if (idCheck == -1)
         {
             // Prompt user for correct ID
             fprintf(stderr, "Please enter a valid alphanumeric ID: ");
             fgets(line, sizeof(line), stdin);
             line[strcspn(line, "\n")] = 0; // Remove newline character
             checkID(line, students, n);
-
         }
-        else if(idCheck == 1)
+        else if (idCheck == 1)
         {
             // Prompt user for unique ID
             fprintf(stderr, "Please enter a unique ID: ");
@@ -43,6 +42,17 @@ void parse_input(int count, FILE *input, FILE *output)
         fgets(line, sizeof(line), input);
         line[strcspn(line, "\n")] = 0; // Remove newline character
 
+        int nameCheck = checkName(line);
+
+        if (nameCheck == 1)
+        {
+            // Prompt user for correct Name
+            fprintf(stderr, "Please enter a valid Name (alphabets and spaces only): ");
+            fgets(line, sizeof(line), stdin);
+            line[strcspn(line, "\n")] = 0; // Remove newline character
+            checkName(line);
+        }
+
         strcpy(students[n].name, line);
 
         for (int i = 0; i < SUBS_COUNT; i++)
@@ -55,16 +65,16 @@ void parse_input(int count, FILE *input, FILE *output)
     }
 
     // Test print all data
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         fprintf(output, "ID: %s\n", students[i].id);
         fprintf(output, "Name: %s\n", students[i].name);
-        for(int j = 0; j < SUBS_COUNT; j++)
+        for (int j = 0; j < SUBS_COUNT; j++)
         {
-            fprintf(output, "Subject %d - Minor: %.2f, Major: %.2f, Total: %.2f\n", 
-                    j + 1, 
-                    students[i].minorScores[j], 
-                    students[i].majorScores[j], 
+            fprintf(output, "Subject %d - Minor: %.2f, Major: %.2f, Total: %.2f\n",
+                    j + 1,
+                    students[i].minorScores[j],
+                    students[i].majorScores[j],
                     students[i].totalScores[j]);
         }
         fprintf(output, "\n");
